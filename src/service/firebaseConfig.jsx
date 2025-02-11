f72f71fd05dda5593cc1555b1022b41ab73cff00
Aditi Ranjan <aditiranjan761@gmail.com>
2025-02-11 21:22:12 +0530

    Initial commit

---
+++ src/service/firebaseConfig.jsx
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAZ-u9JfUL0F5CiEIJwlq9XVZL9WQKSwew",
+    authDomain: "exploria-6a24e.firebaseapp.com",
+    projectId: "exploria-6a24e",
+    storageBucket: "exploria-6a24e.firebasestorage.app",
+    messagingSenderId: "822873635992",
+    appId: "1:822873635992:web:629f2e2ed59fbcad3de4ed",
+    measurementId: "G-2ZNMQKFCZH"
+  };
+
+  // Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
